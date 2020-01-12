c7b5eeb87965a1632d6b782deea8cb7334613369
golovastik <golovastik4044@mail.ru>
2020-01-12 22:19:27 +0300

    WinForm with 3 windows

---
+++ WinFormsApp/LoginForm.cs
@@ -0,0 +1,93 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+
+            this.passField.AutoSize = false;
+            this.passField.Size = new Size(this.passField.Size.Width, 44);
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void closeButton_MouseEnter(object sender, EventArgs e)
+        {
+            closeButton.ForeColor = Color.Red;
+        }
+
+        private void closeButton_MouseLeave(object sender, EventArgs e)
+        {
+            closeButton.ForeColor = Color.Black;
+        }
+
+
+        //тут обработка перемещения экрана
+        Point lastPoint;
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            String login = loginField.Text;
+            String pass = passField.Text;
+
+            DB db = new DB();
+
+            DataTable table = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `usersnames` WHERE `login` = @uL AND `password` = @uP", db.getConnection());
+            command.Parameters.Add("@uL", MySqlDbType.VarChar).Value = login;
+            command.Parameters.Add("@uP", MySqlDbType.VarChar).Value = pass;
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count > 0)
+            {
+                this.Hide();
+                MainForm mainForm = new MainForm();
+                mainForm.Show();
+            }
+            else
+            {
+                MessageBox.Show("Nooooo!!!!");
+            }
+        }
+
+        private void regLabel_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            RegisterForm regForm = new RegisterForm();
+            regForm.Show();
+        }
+    }
+}
---
+++ WinFormsApp/RegisterForm.cs
@@ -0,0 +1,152 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp
+{
+    public partial class RegisterForm : Form
+    {
+        public RegisterForm()
+        {
+            InitializeComponent();
+            userNameField.Text = "Name: ";
+            userNameField.ForeColor = Color.Gray;
+            userSurnameField.Text = "Surname: ";
+            userSurnameField.ForeColor = Color.Gray;
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //тут обработка перемещения экрана
+        Point lastPoint;
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void userNameField_Enter(object sender, EventArgs e)
+        {
+            if(userNameField.Text == "Name: ")
+            {
+                userNameField.Text = "";
+                userNameField.ForeColor = Color.Black;
+            }
+        }
+
+        private void userNameField_Leave(object sender, EventArgs e)
+        {
+            if(userNameField.Text == "")
+            {
+                userNameField.Text = "Name: ";
+                userNameField.ForeColor = Color.Gray;
+            }
+        }
+
+        private void userSurnameField_Leave(object sender, EventArgs e)
+        {
+            if (userSurnameField.Text == "")
+            {
+                userSurnameField.Text = "Surname: ";
+                userSurnameField.ForeColor = Color.Gray;
+            }
+        }
+
+        private void userSurnameField_Enter(object sender, EventArgs e)
+        {
+            if (userSurnameField.Text == "Surname: ")
+            {
+                userSurnameField.Text = "";
+                userSurnameField.ForeColor = Color.Black;
+            }
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            if (userNameField.Text == "Name: ")
+            {
+                MessageBox.Show("Пустое поле имя");
+                return;
+            }
+            if (userSurnameField.Text == "Surname: ")
+            {
+                MessageBox.Show("Пустое поле surname");
+                return;
+            }
+            if (isUserExists())
+            {
+                return;
+            }
+            DB db = new DB();
+            MySqlCommand command = new MySqlCommand("INSERT INTO `usersnames` (`login`, `password`, `Name`, `Surname`) VALUES (@login, @password, @name, @surname)", db.getConnection());
+
+            command.Parameters.Add("@login", MySqlDbType.VarChar).Value = loginField.Text;
+            command.Parameters.Add("@password", MySqlDbType.VarChar).Value = passField.Text;
+            command.Parameters.Add("@name", MySqlDbType.VarChar).Value = userNameField.Text;
+            command.Parameters.Add("@surname", MySqlDbType.VarChar).Value = userSurnameField.Text;
+
+            db.openConnection();
+
+            if (command.ExecuteNonQuery() == 1)
+            {
+                MessageBox.Show("Успех");
+            }
+            else
+            {
+                MessageBox.Show("Noooooo!!!!!");
+            }
+
+            db.closeConnection();
+        }
+
+        public Boolean isUserExists()
+        {
+            DB db = new DB();
+
+            DataTable table = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `usersnames` WHERE `login` = @uL", db.getConnection());
+            command.Parameters.Add("@uL", MySqlDbType.VarChar).Value = loginField.Text;
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count > 0)
+            {
+                MessageBox.Show("login уже есть нужен другой");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void regLabel_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            LoginForm loginForm = new LoginForm();
+            loginForm.Show();
+        }
+    }
+}
