51b1d4b0149a3104cc212cf0db6d1f7aa324aca9
golovastik <golovastik4044@mail.ru>
2020-01-23 20:45:08 +0300

    change bootstrap

--- ShopApplication/Data/mocks/MockBook.cs
+++ ShopApplication/Data/mocks/MockBook.cs
@@ -18,7 +18,7 @@ namespace ShopApplication.Data.mocks
                 {
                     new Book{name = "C# CRL", shortDesc = "прог", longDesc = "много читать", img = "/img/CRL_CS.jpg", price = 32, availabel = true, Category = _categoryBooks.AllCategories.First()},
                     new Book{name = "C# 7.0", shortDesc = "прог", longDesc = "тут ты узнаешь что то новое", img = "/img/CS7.0.jpg", price = 41, availabel = true, Category = _categoryBooks.AllCategories.First()},
-                    new Book{name = "C# .NET Core", shortDesc = "", longDesc = "для супер пупер прогГеров", img = "/img/NETCore.jpg", price = 110, availabel = true, Category = _categoryBooks.AllCategories.First()},
+                    new Book{name = "C# .NET Core", shortDesc = "прог", longDesc = "для супер пупер прогГеров", img = "/img/NETCore.jpg", price = 110, availabel = true, Category = _categoryBooks.AllCategories.First()},
                     new Book{name = "Reack and Morty", shortDesc = "фан", longDesc = "про двух чудаков", img = "/img/randm.jpg", price = 110, availabel = true, Category = _categoryBooks.AllCategories.Last()},
                     new Book{name = "WarCraft", shortDesc = "фан", longDesc = "интересная книга", img = "/img/warcraft.jpg", price = 23, availabel = true, Category = _categoryBooks.AllCategories.Last()}
                 };
