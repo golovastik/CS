13ed00874ff60dd908fcdf5f52df704d3feb693c
golovastik <golovastik4044@mail.ru>
2020-01-23 20:05:42 +0300

    added bootstap

--- ShopApplication/Data/mocks/MockBook.cs
+++ ShopApplication/Data/mocks/MockBook.cs
@@ -16,11 +16,11 @@ namespace ShopApplication.Data.mocks
             {
                 return new List<Book>
                 {
-                    new Book{name = "C# CRL", shortDesc = "прог", longDesc = "много читать", img = "https://cloud.mail.ru/public/Hdia/2Br38EPqg/CRL_CS.jpg", price = 32, availabel = true, Category = _categoryBooks.AllCategories.First()},
-                    new Book{name = "C# 7.0", shortDesc = "прог", longDesc = "тут ты узнаешь что то новое", img = "https://cloud.mail.ru/public/Hdia/2Br38EPqg/CS7.0.jpg", price = 41, availabel = true, Category = _categoryBooks.AllCategories.First()},
-                    new Book{name = "C# .NET Core", shortDesc = "", longDesc = "для супер пупер прогГеров", img = "https://cloud.mail.ru/public/Hdia/2Br38EPqg/NETCore.jpg", price = 110, availabel = true, Category = _categoryBooks.AllCategories.First()},
-                    new Book{name = "Reack and Morty", shortDesc = "фан", longDesc = "про двух чудаков", img = "https://cloud.mail.ru/public/Hdia/2Br38EPqg/randm.jpg", price = 110, availabel = true, Category = _categoryBooks.AllCategories.Last()},
-                    new Book{name = "WarCraft", shortDesc = "фан", longDesc = "интересная книга", img = "https://cloud.mail.ru/public/Hdia/2Br38EPqg/warcraft.jpg", price = 23, availabel = true, Category = _categoryBooks.AllCategories.Last()}
+                    new Book{name = "C# CRL", shortDesc = "прог", longDesc = "много читать", img = "/img/CRL_CS.jpg", price = 32, availabel = true, Category = _categoryBooks.AllCategories.First()},
+                    new Book{name = "C# 7.0", shortDesc = "прог", longDesc = "тут ты узнаешь что то новое", img = "/img/CS7.0.jpg", price = 41, availabel = true, Category = _categoryBooks.AllCategories.First()},
+                    new Book{name = "C# .NET Core", shortDesc = "", longDesc = "для супер пупер прогГеров", img = "/img/NETCore.jpg", price = 110, availabel = true, Category = _categoryBooks.AllCategories.First()},
+                    new Book{name = "Reack and Morty", shortDesc = "фан", longDesc = "про двух чудаков", img = "/img/randm.jpg", price = 110, availabel = true, Category = _categoryBooks.AllCategories.Last()},
+                    new Book{name = "WarCraft", shortDesc = "фан", longDesc = "интересная книга", img = "/img/warcraft.jpg", price = 23, availabel = true, Category = _categoryBooks.AllCategories.Last()}
                 };
             }
         }
